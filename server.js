a3fa97e992c7f563dd1c0ab98d9f5f0ff254c051
shashwat Gupta <shashwat.company@gmail.com>
2019-01-21 16:40:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,193 @@
-module.exports.add = (a, b) => a + b;
 
-module.exports.asyncAdd = (a, b, callback) => {
-  setTimeout(() => {
-    callback(a + b);
-  }, 1000);
+
+
+var express = require('express');
+var app = express();
+
+var cors = require('cors');
+
+app.use(cors());
+
+var linechartData = {
+    title: {
+        text: 'Line Chart'
+    },
+    subtitle: {
+        text: 'BBC Report Team'
+    },
+    series: [{
+        name: 'Installation',
+        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+    }, {
+        name: 'Manufacturing',
+        data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+    }, {
+        name: 'Sales & Distribution',
+        data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+    }, {
+        name: 'Project Development',
+        data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+    }, {
+        name: 'Other',
+        data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+    }],
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 500
+            },
+            chartOptions: {
+                legend: {
+                    layout: 'horizontal',
+                    align: 'center',
+                    verticalAlign: 'bottom'
+                }
+            }
+        }]
+    }
 };
 
-module.exports.square = (x) => x * x;
+var areaChartData = {
+    chart: {
+        type: 'area'
+    },
+    title: {
+        text: 'Area Chart'
+    },
+    subtitle: {
+        text: 'BBC Report Team'
+    },
+    xAxis: {
+        categories: ['1750', '1800', '1850', '1900', '1950', '1999', '2050'],
+        tickmarkPlacement: 'on',
+        title: {
+            enabled: false
+        }
+    },
 
-module.exports.asyncSquare = (x, callback) => {
-  setTimeout(() => {
-    callback(x * x);
-  }, 1000);
+    tooltip: {
+        split: true,
+        valueSuffix: ' millions'
+    },
+    plotOptions: {
+        area: {
+            stacking: 'normal',
+            lineColor: '#666666',
+            lineWidth: 1,
+            marker: {
+                lineWidth: 1,
+                lineColor: '#666666'
+            }
+        }
+    },
+    series: [{
+        name: 'Asia',
+        data: [502, 635, 809, 947, 1402, 3634, 5268]
+    }, {
+        name: 'Africa',
+        data: [106, 107, 111, 133, 221, 767, 1766]
+    }, {
+        name: 'Europe',
+        data: [163, 203, 276, 408, 547, 729, 628]
+    }, {
+        name: 'America',
+        data: [18, 31, 54, 156, 339, 818, 1201]
+    }, {
+        name: 'Oceania',
+        data: [2, 2, 2, 6, 13, 30, 46]
+    }],
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 500
+            },
+            chartOptions: {
+                legend: {
+                    layout: 'horizontal',
+                    align: 'center',
+                    verticalAlign: 'bottom'
+                }
+            }
+        }]
+    }
 };
 
-module.exports.setName = (user, fullName) => {
-  var names = fullName.split(' ');
-  user.firstName = names[0];
-  user.lastName = names[1];
-  return user;
+var PiechartData = {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Pie chart'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: false
+            },
+            showInLegend: true
+        }
+    },
+    series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+            name: 'Chrome',
+            y: 61.41,
+            sliced: true,
+            selected: true
+        }, {
+            name: 'Internet Explorer',
+            y: 11.84
+        }, {
+            name: 'Firefox',
+            y: 10.85
+        }, {
+            name: 'Edge',
+            y: 4.67
+        }, {
+            name: 'Safari',
+            y: 4.18
+        }, {
+            name: 'Other',
+            y: 7.05
+        }]
+    }]
 };
+
+
+app.get('/fetchData', function (req, res) {
+    console.log(req.params['chartName']);
+    switch (req.params['chartName']) {
+        case 'line':
+          return res.send(linechartData);
+          break; 
+        case 'area':
+           return res.send(areaChartData);
+          break; 
+        case 'pie':
+         return res.send(PiechartData);
+        break; 
+
+        default : 
+        return res.send(linechartData);
+      }
+  
+})
+
+var server = app.listen(3000, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+});
+ 
+var mockserver = app;
+module.exports = mockserver
