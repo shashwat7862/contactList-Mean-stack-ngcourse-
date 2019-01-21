94759737b6cf00acd69987749ae75b6f0605c016
shashwat Gupta <shashwat.company@gmail.com>
2019-01-21 16:18:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
-var express = require('express');
+module.exports.add = (a, b) => a + b;
 
-var app = express();
- 
- 
+module.exports.asyncAdd = (a, b, callback) => {
+  setTimeout(() => {
+    callback(a + b);
+  }, 1000);
+};
 
+module.exports.square = (x) => x * x;
 
- 
- 
+module.exports.asyncSquare = (x, callback) => {
+  setTimeout(() => {
+    callback(x * x);
+  }, 1000);
+};
 
-app.get('/',function(req,res)
-{
- 
- 
-   
-    console.log("get api call");
-	res.send("called api")
-
-
-});
-
-
-
-app.listen(3000);
-console.log('server running');
+module.exports.setName = (user, fullName) => {
+  var names = fullName.split(' ');
+  user.firstName = names[0];
+  user.lastName = names[1];
+  return user;
+};
